a9f386b239fc3a0722e557bc791a4e5a815cd747
Shahmir <>
2021-04-29 09:02:52 +0000

    done

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,19 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
+    int count = 0;
+for(int row = 0;row<nameArray.length;row++)
+{
+for(int col = 0;col<nameArray[0].length;col++)
+{
+String[] name = nameArray[row][col].split(",");
+if(name[0].equals(lastName))
+{
+count++;
+}
+}
+}
+return count;
 
-
-    return 0;
   }
 }
